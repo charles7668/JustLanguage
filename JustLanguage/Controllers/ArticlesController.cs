7712e6a77484223cdd709a4f84608b32fa3529ed
charles <charles76680119@gmail.com>
2024-02-19 06:07:03 +0800

    [feat] Define a POST api 'Articles'
    this API will upload a site information to parse article information using parse rule

---
+++ JustLanguage/Controllers/ArticlesController.cs
@@ -0,0 +1,28 @@
+﻿using JustLanguage.DataBase;
+using JustLanguage.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JustLanguage.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ArticlesController : Controller
+{
+    public ArticlesController(ILogger<ArticlesController> logger, IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+        _logger = logger;
+    }
+
+    private readonly ILogger<ArticlesController> _logger;
+    private readonly IUnitOfWork _unitOfWork;
+
+    [HttpPost]
+    public Task<IActionResult> Post([FromBody] UploadArticleDTO dto)
+    {
+        _logger.LogInformation("input : {@DTO}", dto);
+        // TODO using XPath to parse article
+
+        return Task.FromResult<IActionResult>(Ok());
+    }
+}
---
+++ JustLanguage/DTOs/UploadArticleDTO.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JustLanguage.DTOs;
+
+public class UploadArticleDTO
+{
+    [Url(ErrorMessage = "article url is not url format")]
+    public string ArticleUrl { get; set; } = string.Empty;
+}
