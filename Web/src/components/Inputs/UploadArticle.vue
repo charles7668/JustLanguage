1e857d8c35192c83e34349e63db21bc333d9ebf8
charles <charles76680119@gmail.com>
2024-02-20 10:54:58 +0800

    [UI] Add upload article button and dialog

---
+++ Web/src/components/Inputs/UploadArticle.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+
+const props = defineProps({
+  CloseAction: {
+    type: Function,
+    required: true
+  }
+})
+
+const uploadAction = () => {
+  props.CloseAction()
+}
+
+</script>
+
+<template>
+  <v-card>
+    <v-card-title>
+      Upload Article
+    </v-card-title>
+    <v-card-item>
+      <v-responsive
+          class="mx-auto"
+          width="500px">
+        <v-text-field></v-text-field>
+      </v-responsive>
+    </v-card-item>
+    <v-card-actions>
+      <v-row>
+        <v-spacer></v-spacer>
+        <v-btn text="Upload" @click="uploadAction"></v-btn>
+        <v-btn text="Cancel" @click="props.CloseAction()"></v-btn>
+        <v-spacer></v-spacer>
+      </v-row>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<style scoped>
+
+</style>
--- Web/src/components/MainPage/ActionBar.vue
+++ Web/src/components/MainPage/ActionBar.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import {ref} from 'vue'
 import UploadParseRule from '../Inputs/ManageParseRules.vue'
+import UploadArticle from "@/components/Inputs/UploadArticle.vue";
 
 let dialog = ref(false)
+let uploadArticleDialog = ref(false)
 
 const closeDialog = () => {
   dialog.value = false
+}
+
+const closeUploadArticleDialog = () => {
+  uploadArticleDialog.value = false
 }
 </script>
 
@@ -14,9 +20,15 @@ const closeDialog = () => {
     <v-app-bar app color="cyan">
       <v-row justify="center">
         <v-btn variant="outlined"
-          >MANAGE RULES
+        >MANAGE RULES
           <v-dialog v-model="dialog" activator="parent" width="1000px" persistent>
-            <UploadParseRule :CloseAction="closeDialog" />
+            <UploadParseRule :CloseAction="closeDialog"/>
+          </v-dialog>
+        </v-btn>
+        <v-btn variant="outlined">
+          UPLOAD ARTICLE
+          <v-dialog v-model="uploadArticleDialog" activator="parent" width="auto" persistent>
+            <UploadArticle :CloseAction="closeUploadArticleDialog"></UploadArticle>
           </v-dialog>
         </v-btn>
       </v-row>
